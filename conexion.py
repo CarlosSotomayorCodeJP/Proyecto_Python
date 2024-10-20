02a9dc6f8f7659d98aeb8911c9bcbe694229a225
CarlosSotomayorCodeJP <carlosmanuelsotomayorbeltran@gmail.com>
2024-10-19 23:43:04 -0400

    Creacion de usuario

--- conexion.py
+++ conexion.py
@@ -3,7 +3,7 @@ class Conexion():
     def __init__(self):
         try:
             self.con=sqlite3.connect("banco.db")
-            self.crearTabalas()
+            self.crearTablas()
         except Exception as ex:
             print(ex)
 
@@ -11,4 +11,15 @@ class Conexion():
         sql_tabla1= """CREATE TABLE IF NOT EXISTS usuarios(id INTEGER PRIMARY KEY AUTOINCREMENT,nombre TEXT, usuario TEX UNIQUE, clave TEXT)"""
         cur=self.con.cursor()
         cur.execute(sql_tabla1)
+        cur.close()
+        self.crearAdmin()
+
+    def crearAdmin(self):
+        try:
+            sql_insert= """INSERT INTO usuarios values(null, '{}', '{}', '{}')""".format("Administrador", "admin", "admin2050.")
+            cur=self.con.cursor()
+            cur.execute(sql_insert)
+            self.con.commit()
+        except Exception as ex:
+            print("Usuario existente",ex) 
 con=Conexion()
