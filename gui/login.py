bc963357667c296872b5b7810e636465cfc9777e
CarlosSotomayorCodeJP <carlosmanuelsotomayorbeltran@gmail.com>
2024-10-31 00:51:10 -0400

    finalizacion del login del Usuario

--- gui/login.py
+++ gui/login.py
@@ -1,24 +1,34 @@
 from PyQt6 import uic 
 from PyQt6.QtWidgets import QMessageBox
 
+from data.user import UsuarioDate
+from model.usuario import Usuario
+
 #from PyQt6 import uic 
 class Login():
     def __init__(self):
         self.login = uic. loadUi("gui/login.ui")
         self.initGUI()
-        self.login.lblMensaje.setText("")
+        self.login.Mensaje.setText("")
         self.login.show()
 
     def ingresar (self):
-        if len(self.login.txtUsuario.text() ) < 2:
-             self.login.labelMensaje.setText("Ingrese un mesaje valido")
-             self.login.txtUsuario.setFocus()
-        elif len(self.login.txtClave.text() ) < 3:
-            self.login.labelMensaje.setText("Ingrese una contraseña valida")
-            self.login.txtClave.setFocus()
+        if len(self.login.Usuario.text() ) < 2:
+             self.login.Mensaje.setText("Ingrese Nombre valido")
+             self.login.Usuario.setFocus()
+        elif len(self.login.Clave.text() ) < 3:
+            self.login.Mensaje.setText("Ingrese una contraseña valida")
+            self.login.Clave.setFocus()
         else:
-            self.login.labelMensaje.setText("")
-            pass 
+            self.login.Mensaje.setText("")
+            usu=Usuario(usuario=self.login.Usuario.text(), clave=self.login.Clave.text())
+            usuDate=UsuarioDate()
+            res=usuDate.login(usu)
+            if res:
+                 self.login.Mensaje.setText("OK")
+            else:
+                 self.login.Mensaje.setText("Datos de acceso incorrecto")
+
 
     def initGUI(self):
-        self.login.btnAcceder.clicked.connect(self.ingresar)
+        self.login.Acceder.clicked.connect(self.ingresar)
